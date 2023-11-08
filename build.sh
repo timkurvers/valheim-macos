aeeb3402a70aa574f252412fefd368ee9b5d3ec6
Ilya Otyutskiy <sharp@thesharp.org>
2023-11-08 23:39:41 +0200

    feat: support patch 0.217.29 for stable and beta

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ depotid=892971
 
 # Stable (public)
 branch="public"
-buildid=12593117
-manifestid=8248830790758710826
-version="0.217.28"
+buildid=12651289
+manifestid=6680211325088078420
+version="0.217.29"
 depotdownloaderversion="2.5.0"
 depotdownloaderhash="462442ad9973c6482be6a1a0af3aee60"
 unityversion="2022.3.12f1"
@@ -28,9 +28,9 @@ outdir="build"
 # Beta (public-test)
 if [[ " $* " =~ " --beta " ]]; then
   branch="public-test"
-  buildid=12593117
+  buildid=12651289
   unset manifestid
-  version="0.217.28"
+  version="0.217.29"
   outdir="build-beta"
 fi
 
